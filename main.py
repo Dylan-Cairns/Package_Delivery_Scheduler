b62a95ccb0b340940e712e1f0fd184096bf4934e
diyanious <dcairns@wgu.edu>
2020-07-15 17:27:52 +0800

    import of all csv data working

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ def main():
     if __name__ == "__main__":
         main()
 
-hash_map = HashMap()
+
+packages_list = model.import_packages_from_CSV()
+#packages_list.print_packages_list()
+
+package1 = packages_list.get_package(5)
+print(package1)
+
+location_dictionary = model.LocationDictionary()
+print(location_dictionary.get_location_id(package1.get_address()))
+print(location_dictionary.get_address(19))
